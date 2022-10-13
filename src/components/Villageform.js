89f124c04d3a99672f01c7c19de3bf7bf8802eb1
Your Name <you@example.com>
2022-10-13 01:15:16 -0700

    new done

--- src/components/Villageform.js
+++ src/components/Villageform.js
@@ -2,8 +2,6 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import { Button } from '@mui/material';
 import { Container } from '@mui/system';
 import FormattedInputs from './idCard';
@@ -116,12 +114,7 @@ export default function VillageForm() {
           />
         </Grid>
         <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes"  />
-            }
-            label="Use this address for further details"
-
-           />
+          
            <Button type='submit' variant='contained'><Link to='/dash' style={{ textDecoration: 'none',color:'white',}}>Submit</Link></Button>
         </Grid>
       </Grid>
